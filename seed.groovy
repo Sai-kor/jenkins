4f404955909f82611ed5526ac094543ff14fb40d
sai k <psai0073@gmail.com>
2022-02-28 08:38:28 -0500

    cart pipeline seed job

--- seed.groovy
+++ seed.groovy
@@ -39,3 +39,28 @@ pipelineJob('roboshop-ansible') {
     }
   }
 }
+// creating folder within that ansible roboshop job
+folder('CI pipelines') {
+  displayName('CI pipelines')
+  description('CI pipelines')
+}
+pipelineJob('CI pipeline/cart') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Sai-kor/cart.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}

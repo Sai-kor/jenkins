87b5e9852d952f329dd409aaa6ae331d8e798c0a
sai k <psai0073@gmail.com>
2022-02-28 23:38:36 -0500

    def call()

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,27 +1,29 @@
-pipeline{
-    agent{
-        label 'WORKSTATION'
-    }
-    //Auto trigger
-    triggers {
-        pollSCM('*/2 * * * *')
-    }
-    stages{
-        stage('compile the code'){
-            steps {
-                sh 'echo compile the code'
-            }
+def call(){
+    pipeline{
+        agent{
+            label 'WORKSTATION'
         }
-        stage('check the code quality'){
-            steps{
-                sh 'echo check the code quality'
-            }
+        //Auto trigger
+        triggers {
+            pollSCM('*/2 * * * *')
         }
-        stage('Test cases'){
-            steps{
-                sh 'echo Test cases'
+        stages{
+            stage('compile the code'){
+                steps {
+                    sh 'echo compile the code'
+                }
             }
-        }
+            stage('check the code quality'){
+                steps{
+                    sh 'echo check the code quality'
+                }
+            }
+            stage('Test cases'){
+                steps{
+                    sh 'echo Test cases'
+                }
+            }
 
+        }
     }
 }

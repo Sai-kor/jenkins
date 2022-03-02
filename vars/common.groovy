6c12496a764d02cf9ae6d1a19e50b3bd04502672
sai k <psai0073@gmail.com>
2022-03-01 23:32:35 -0500

    authenticate sonarqube user pswd

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,3 @@
 def sonarQube(){
-    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.3.23:9000'
+    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.3.23:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 }

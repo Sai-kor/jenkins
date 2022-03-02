85bff2aa6376a06cc9ed00693464c3779d6e193d
sai k <psai0073@gmail.com>
2022-03-02 01:14:44 -0500

    sonar quality gate api for sonarqube status check

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,6 @@
 def sonarQube(){
     sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.3.23:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+    //check whether the submission went fine or not- for this sonar-scanner cannot help but a api curl command can help
+    sh 'sonar-quality-gate.sh admin:admin123 172.31.3.23:9000 ${COMPONENT}'
+
 }

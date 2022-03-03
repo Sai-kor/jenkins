3b48b576a657e2dc3e4c1ab95981dbd78c6ad226
sai k <psai0073@gmail.com>
2022-03-03 02:04:39 -0500

    import utils class

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,5 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
 def sonarQube(){
     //sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.3.23:9000 -Dsonar.login=admin -Dsonar.password=admin123'
     //check whether the submission went fine or not- for this sonar-scanner cannot help but a api curl command can help

9959d5291aa4ba05ff645ca8ff88d8a5cd4ba982
sai k <psai0073@gmail.com>
2022-03-08 15:10:02 -0500

    seed job for terraform

--- seed.groovy
+++ seed.groovy
@@ -71,3 +71,29 @@ for(i in 0..SIZE)  {
     }
   }
 }
+
+folder('Mutable') {
+  displayName('Mutable')
+  description('Mutable')
+}
+
+pipelineJob('terraform-vpc') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Sai-kor/terraform-vpc.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
